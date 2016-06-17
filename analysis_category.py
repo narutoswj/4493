72d3f649f1cf38bb3157337d238772850cca483b
narutoswj <narutoswj@hotmail.com>
2016-06-17 15:13:10 +0800

    update

--- analysis_category.py
+++ analysis_category.py
@@ -6,41 +6,10 @@ import os
 import sys
 
 from pyquery import PyQuery as pq
-
-def Get_Category():
-    client = pymongo.MongoClient(host="127.0.0.1", port=27017)
-    # Connect Database
-    db = client.beauty
-    # Use collection
-    collection = db['4493_category']
-    category = collection.find({})
-    client.close()
-    return category
-
-def Get_Category_withtotal():
-    client = pymongo.MongoClient(host="127.0.0.1", port=27017)
-    # Connect Database
-    db = client.beauty
-    # Use collection
-    collection = db['4493_category']
-    category = Get_Category()
-    for cat in category:
-        first_page = cat['url']
-        print first_page
-        try:
-            total_images = pq(first_page)('.interestline')('span:first').html()
-            if not total_images == None:
-                collection.update({"_id": cat["_id"]}, {"$set": {"total_images": total_images}})
-        except Exception, e:
-            print e
-    client.close()
-
-client = pymongo.MongoClient(host="127.0.0.1", port=27017)
-# Connect Database
-db = client.beauty
-# Use collection
-collection = db['4493_category']
-category = collection.find({""})
-
-
-Get_Category_withtotal()
+print (pq('http://www.4493.com/xingganmote/index-2.htm')('.clearfix').html())
+html = pq('http://www.4493.com/xingganmote/index-2.htm')('.clearfix')('li')
+j = 1
+for i in html:
+    print j
+    print pq(i).html()
+    j = j + 1
